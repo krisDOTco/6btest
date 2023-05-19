9f2be99cb9d98ced7cd59e990bb88f07d1181ad4
Kris Young <kris@haust.co.uk>
2023-05-19 19:25:24 +0100

    Implement base theme

---
+++ wp-content/themes/6btest/index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Theme Name: 6b Test
+ * Theme URI: http://6btest.local
+ * Description: A starter theme for WordPress.
+ * Version: 1.0
+ * Author: Kris Young
+ * Author URI: https://www.kris.co
+ * Text Domain: 6btest
+ */
